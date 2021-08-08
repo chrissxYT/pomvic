0023c486dfe12d9744017c729a8cc82d2d3a0235
chris <chrissx@chrissx.de>
2021-08-08 06:09:57 +0200

    More work...
    - Better macros for the "key commands" (normal and visual modes)
    - Implemented `x`, `X`, `i`, `a` and `0` "key commands"
    - Implemented Escape and Return on Ex Mode

--- pomvic.hh
+++ pomvic.hh
@@ -45,12 +45,14 @@ struct cursor_t {
 struct buffer {
         cursor_t cursor;
         std::vector<vistring> content;
+        // TODO: move this into the cursor
         vistring _buffer;
         vistring newline;
 #ifdef POMVIC_INTERNAL_YANK
         vistring yankboard;
 #endif
 };
+// TODO: struct vi { buffer b; cursor c; };
 std::vector<vistring> split_by_newlines(vistring raw, vistring *newline) {
         vistring nl;
         if(!newline) newline = &nl;
@@ -88,63 +90,84 @@ void yank(buffer &b, vistring s, bool e) {
         }
 #endif
 }
-#define cmd(x) (s == x)
-#define mod_cmd(x, y) cmd(x && (m & y))
-#define none_cmd(x) cmd(x && !m)
+inline bool _cmd(vistring s, std::string x) {
+        return s == vistring(x.begin(), x.end());
+}
+#define cmd(x) (_cmd(s, #x))
+#define mod_cmd(x, y) (cmd(x) && (m & y))
+#define none_cmd(x) (cmd(x) && !m)
 #define ctrl_cmd(x) mod_cmd(x, MOD_CTRL)
 #define meta_cmd(x) mod_cmd(x, MOD_META)
 #define cmd_cmd(x) mod_cmd(x, MOD_CMD)
+#define group(x) (s[0] == (vichar)#x[0])
 void handle_key_command(buffer &b, vichar c, modifier m) {
         auto &s = b._buffer += c;
-        if(none_cmd(U"x")) {
-                s.erase(b.under_cursor());
-        } else if(none_cmd(U"X")) {
-                s.erase(b.under_cursor() - 1);
-        } else if(none_cmd(U"v")) {
+        if(none_cmd(x)) {
+                b.content[b.cursor.pos.y].erase(b.cursor.pos.x);
+        } else if(none_cmd(X)) {
+                // TODO: check if this can throw
+                b.content[b.cursor.pos.y].erase(b.cursor.pos.x - 1);
+        } else if(none_cmd(i)) {
+                b._buffer.clear();
+                b.cursor.mode = MODE_INSERT;
+        } else if(none_cmd(I)) {
+                // TODO
+        } else if(none_cmd(a)) {
+                b._buffer.clear();
+                b.cursor.mode = MODE_INSERT;
+                b.cursor.pos.x++;
+        } else if(none_cmd(A)) {
+                // TODO: $a
+        } else if(none_cmd(v)) {
                 if(b.cursor.mode == MODE_VISUAL) {
                         b.cursor.mode = MODE_NORMAL;
                 } else {
                         b.cursor.visual_origin = b.cursor.pos;
                         b.cursor.mode = MODE_VISUAL;
                 }
-        } else if(none_cmd(U"V")) {
+        } else if(none_cmd(V)) {
                 if(b.cursor.mode == MODE_VISUAL_LINE) {
                         b.cursor.mode = MODE_NORMAL;
                 } else {
                         b.cursor.visual_origin = b.cursor.pos;
                         b.cursor.mode = MODE_VISUAL_LINE;
                 }
-        } else if(ctrl_cmd(U"v") || ctrl_cmd(U"V")) {
+        } else if(ctrl_cmd(v) || ctrl_cmd(V)) {
                 if(b.cursor.mode == MODE_VISUAL_BLOCK) {
                         b.cursor.mode = MODE_NORMAL;
                 } else {
                         b.cursor.visual_origin = b.cursor.pos;
                         b.cursor.mode = MODE_VISUAL_BLOCK;
                 }
-        } else if(none_cmd(U"w")) {
+        } else if(none_cmd(w)) {
                 // TODO: forward one word
-        } else if(none_cmd(U"W")) {
+        } else if(none_cmd(W)) {
                 // TODO: forward one word
-        } else if(none_cmd(U"b")) {
+        } else if(none_cmd(b)) {
                 // TODO: back one word
-        } else if(none_cmd(U"B")) {
+        } else if(none_cmd(B)) {
                 // TODO: back one word
-        } else if(none_cmd(U"h")) {
+        } else if(none_cmd(h)) {
                 if(b.cursor.pos.x > 0) {
                         b.cursor.pos.x--;
                 }
-        } else if(none_cmd(U"j")) {
+        } else if(none_cmd(j)) {
                 if(b.cursor.pos.y < b.content.size() - 1) {
                         b.cursor.pos.x++;
                 }
-        } else if(none_cmd(U"k")) {
+        } else if(none_cmd(k)) {
                 if(b.cursor.pos.y > 0) {
                         b.cursor.pos.y--;
                 }
-        } else if(none_cmd(U"l")) {
+        } else if(none_cmd(l)) {
                 if(b.cursor.pos.x < b.content[b.cursor.pos.y].size() - 1) {
                         b.cursor.pos.x++;
                 }
+        } else if(none_cmd(0)) {
+                b.cursor.pos.x = 0;
+        } else if (group(c)) {
+        } else if (group(d)) {
+        } else if (none_cmd(gg)) {
         }
         else {
                 return;
@@ -157,6 +180,10 @@ void handle_key_command(buffer &b, vichar c, modifier m) {
 #undef ctrl_cmd
 #undef meta_cmd
 #undef cmd_cmd
+#undef group
+void run_ex_command(buffer &b, vistring cmd) {
+        // TODO: ...
+}
 void update(buffer &b, vichar c, modifier m) {
         switch(b.cursor.mode) {
                 case MODE_NORMAL:
@@ -174,9 +201,12 @@ void update(buffer &b, vichar c, modifier m) {
                         break;
                 case MODE_EX:
                         if(m & MOD_ESC) {
-                                // TODO:close
+                                b._buffer.clear();
+                                b.cursor.mode = MODE_NORMAL;
                         } else if(m & MOD_RET) {
-                                // TODO: run command
+                                run_ex_command(b, b._buffer);
+                                b._buffer.clear();
+                                b.cursor.mode = MODE_NORMAL;
                         } else {
                                 b._buffer += c;
                         }
